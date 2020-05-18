fec104929027c7fb6f4099b1be405d69fd6c49a6
Rezaaisyi <55181853+Rezaaisyi@users.noreply.github.com>
2020-05-18 03:15:25 -0400

    Add files via upload

---
+++ app/core/Constants.php
@@ -0,0 +1,8 @@
+<?php
+
+define("BASE_URL", 'http://localhost:81/phpmvc2/public');
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "Donasi");
+define("DB_USERNAME", "integratif");
+define("DB_PASSWORD", "integratif");
